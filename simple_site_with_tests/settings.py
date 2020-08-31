17ca704368110c5bc79b8de365c0649c56b9e155
marnikvla <condor52@yandex.ru>
2020-08-31 15:41:45 +0300

    add tests app

--- simple_site_with_tests/settings.py
+++ simple_site_with_tests/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'simple_site_with_tests.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [Path(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
