8f8014cba2c3db8a5eb9e780d1538568a9342d7b
marnikvla <condor52@yandex.ru>
2020-09-03 16:02:11 +0300

    add urls

--- simple_site_with_tests/urls.py
+++ simple_site_with_tests/urls.py
@@ -1,21 +1,15 @@
-"""simple_site_with_tests URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from tests_app.views import Index
 
 urlpatterns = [
+    path('', Index.as_view(), name='index'),
     path('admin/', admin.site.urls),
+    path('tests/', include('tests_app.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                           document_root = settings.STATIC_ROOT)
