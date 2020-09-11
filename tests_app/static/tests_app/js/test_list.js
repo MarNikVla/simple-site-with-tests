1e53fa2fc876ac0d701204a97219bae1c36eb1e9
marnikvla <condor52@yandex.ru>
2020-09-11 14:53:06 +0300

    working version jof getChecked function js

--- tests_app/static/tests_app/js/test_list.js
+++ tests_app/static/tests_app/js/test_list.js
@@ -1,96 +1,32 @@
-function MyFuncd() {
-    let result = false;
+// getAllChecked проверяет заполнены(выбраны) все ответы в вопросах (input)
+// возвращает true если заполнены все, иначе false
+function getAllChecked(selector) {
+    let result = true;
+    // getChecked проверяет заполнен(выбран) отдельный ответ (input)
+    // возвращает true если заполнены все, иначе false
     function getChecked() {
-        // let result = "n";
-        console.log($(this))
+        console.log($(this));
+        // check = false
         $(this).each(function () {
             if ($(this).find('input:checked').val() !== undefined) {
                 console.log($(this).find('input:checked').val());
-                // console.log('dsfs')
-                // $('#submit').attr('disabled', false)
                 result = result && true
-
-            } else {
-                result = result && false;
-                console.log('dsfs')
-                // $('#submit').attr('disabled', true)
+            }else {
+                result = result && false
             }
-
-
+            return result
         });
-
-
     }
-    // if (result==true) {
-    //     $('#submit').attr('disabled', false)
-    // } else {
-    //     $('#submit').removeAttr('disabled', true)
-    // }
-    $('.custom-radio[id="2"], .custom-radio[id="3"]').each(getChecked)
-// console.log(Boolean(result))
-    console.log(Boolean(result))
+    $(selector).each(getChecked)
+
+    return result
 }
 
-// setInterval(MyFuncd, 1000)
-
-// let result = false;
-// function getChecked() {
-//     // let result = "n";
-//     console.log($(this))
-//     $(this).each(function () {
-//         if ($(this).find('input:checked').val() !== undefined) {
-//             console.log($(this).find('input:checked').val());
-//             // console.log('dsfs')
-//             // $('#submit').attr('disabled', false)
-//             result =result|| true
-//
-//         } else {
-//             result = result && false;
-//             console.log('dsfs')
-//             // $('#submit').attr('disabled', true)
-//         }
-//
-//
-//     });
-//
-// }
-
-// $('.custom-radio[id="2"], .custom-radio[id="3"]').on('input', getChecked);
-//
-// $('.custom-radio[id="2"], .custom-radio[id="3"]').each(getChecked)
-// console.log(Boolean(result))
-// function MyFuc(){
-//     all_res=false
-// }
-
-// all_res=false
-// all_res+=$('.custom-radio[id="2"], .custom-radio[id="3"]').each(getChecked)
-// console.log(all_res)
-
-// setInterval(getChecked,500)
-// $('.custom-radio[id="2"]').on('input change', function () {
-//     let all_checked = false
-//     $(this).each(function () {
-//
-//         if ($(this).children('input').is(':checked')) {
-//
-//             all_checked = all_checked || true
-//             console.log($(this).children('input'))
-//             console.log(all_checked)
-//         }
-//
-//         console.log(all_checked)
-//     }
-//     );
-// });
-//
-// $(function () {
-//     $(":input").each(function () {
-//         log.console("6")
-//         if ($(this).val() == '') {
-//             $('.btn[type="submit"]').prop('disabled', true);
-//         } else {
-//             $('.btn[type="submit"]').prop('disabled', false);
-//         }
-//     });
-// });
+function changeAttrSubmit(selector) {
+    if (getAllChecked(selector) == true) {
+        $('#submit').removeAttr('disabled', true)
+    } else {
+        $('#submit').attr('disabled', true)
+    }
+}
+setInterval(changeAttrSubmit, 2000, ('.custom-radio[id]'));
