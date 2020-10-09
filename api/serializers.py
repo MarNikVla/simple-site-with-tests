d197b1bbe7d4d8442c9448a736a03538a36613ca
marnikvla <condor52@yandex.ru>
2020-10-09 13:23:46 +0300

    add api permissions

--- api/serializers.py
+++ api/serializers.py
@@ -1,26 +1,20 @@
-from rest_framework import  serializers
+from rest_framework import serializers
 
 
 from tests_app.models import TestCategory, Ticket, Question
+
 
 class TicketSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Ticket
         fields = '__all__'
 
+
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
-    # ticket123 = TicketSerializer(many=True, read_only=True)
+
     class Meta:
         model = TestCategory
         fields = '__all__'
-
-
-        # lookup_field = 'slug'
-        # extra_kwargs = {
-        #     'url': {'lookup_field': 'slug'}
-        # }
-
-
 
 
 class QuestionSerializer(serializers.HyperlinkedModelSerializer):
--- api/views.py
+++ api/views.py
@@ -2,65 +2,45 @@ from django.shortcuts import render
 from rest_framework import viewsets, generics
 from rest_framework.decorators import api_view, action
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from api.serializers import CategorySerializer, TicketSerializer, QuestionSerializer
 from tests_app.models import TestCategory, Ticket, Question
 
-# @api_view (['GET'])
-# def bbs(request):
-#     if request.method == 'GET':
-#         bbs = Bb.objects.filter(is_active=True)[:10]
-#         serializer = BbSerializer(bbs, many=True)
-#         return Response(serializer.data)
 
-
-class CategoryViewSet(viewsets.ModelViewSet) :
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = TestCategory.objects.all()
     serializer_class = CategorySerializer
-
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     @action(detail=True, methods=['get'],
             serializer_class=TicketSerializer,
             # queryset=Ticket.objects.all()
             # authentication_classes=[BasicAuthentication],
-            # permission_classes=[IsAuthenticated, IsEnrolled],
+            permission_classes=[IsAuthenticatedOrReadOnly],
             )
     def tickets(self, request, *args, **kwargs):
         self.queryset = Ticket.objects.filter(category__exact=self.kwargs['pk'])
         return self.list(request, *args, **kwargs)
 
-    # lookup_field = 'slug'
 
-class TicketViewSet(viewsets.ModelViewSet) :
+class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     @action(detail=True, methods=['get'],
             serializer_class=QuestionSerializer,
             # queryset=Ticket.objects.all()
             # authentication_classes=[BasicAuthentication],
-            # permission_classes=[IsAuthenticated, IsEnrolled],
+            permission_classes=[IsAuthenticatedOrReadOnly],
             )
     def questions(self, request, *args, **kwargs):
         self.queryset = Question.objects.filter(ticket__exact=self.kwargs['pk'])
         return self.list(request, *args, **kwargs)
-    # def get_queryset(self):
-    #     queryset = Ticket.objects.filter(category__exact='2')
-    #     # queryset=super().get_queryset()
-    #     return queryset
 
-    # print('hgjg')
 
-    # @action(detail=True, methods=['get'],
-    #         serializer_class=QuestionSerializer,
-    #         # queryset=Ticket.objects.all()
-    #         # authentication_classes=[BasicAuthentication],
-    #         # permission_classes=[IsAuthenticated, IsEnrolled],
-    #         )
-    # def questions(self, request, *args, **kwargs):
-    #     self.queryset = Question.objects.filter(ticket__exact=self.kwargs['pk'])
-    #     return self.list(request, *args, **kwargs)
-
-class QuestionViewSet(viewsets.ModelViewSet) :
+class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
